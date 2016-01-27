f581e000917238ee6991cc56bd1f6b871e4ec8fe
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-27 11:38:20 +0000

    added bin files

---
+++ bin/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd "`dirname $0`"
+
+cd ../web
+
+rm -r starthardware.org
+
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --span-hosts \
+     --domains='*.gravatar.com,ajax.googleapis.com,fonts.googleapis.com,starthardware.org,www.googletagmanager.com' \
+     --exclude-domains=www.youtube.com,sciencehackday2015.eventbrite.com \
+     http://starthardware.org
