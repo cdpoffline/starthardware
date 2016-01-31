581a28695a268152e948e40bfe6a33789e998bc9
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-31 14:22:15 +0000

    added download to site directory

--- bin/update.sh
+++ bin/update.sh
@@ -6,9 +6,15 @@ cd "`dirname $0`"
 
 cd ../web
 
-rm -r starthardware.org
+mkdir -p "site"
+
+cd site
 
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent --span-hosts \
      --domains='*.gravatar.com,ajax.googleapis.com,fonts.googleapis.com,starthardware.org,www.googletagmanager.com' \
      --exclude-domains=www.youtube.com,sciencehackday2015.eventbrite.com \
+     -c -N \
+     --header='User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0' \
+     --header='Cookie: _icl_current_language=de' \
+     --header='Referer: http://starthardware.org/' \
      http://starthardware.org
