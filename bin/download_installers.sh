8833195c8d86d35ef089ae692cd0e7424c368d82
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-31 14:20:41 +0000

    only downloadig when newer

--- bin/download_installers.sh
+++ bin/download_installers.sh
@@ -4,14 +4,13 @@ cd "`dirname $0`"
 
 cd ../web
 
-rm -r downloads
-mkdir downloads
+mkdir -p downloads
 cd downloads
 
 version=1.6.7
 
-wget https://downloads.arduino.cc/arduino-${version}-linux64.tar.xz
-wget https://downloads.arduino.cc/arduino-${version}-windows.exe
-wget https://downloads.arduino.cc/arduino-${version}-windows.zip
-wget https://downloads.arduino.cc/arduino-${version}-macosx.zip
-wget https://downloads.arduino.cc/arduino-${version}-linux32.tar.xz
+wget -c -N https://downloads.arduino.cc/arduino-${version}-linux64.tar.xz
+wget -c -N https://downloads.arduino.cc/arduino-${version}-windows.exe
+wget -c -N https://downloads.arduino.cc/arduino-${version}-windows.zip
+wget -c -N https://downloads.arduino.cc/arduino-${version}-macosx.zip
+wget -c -N https://downloads.arduino.cc/arduino-${version}-linux32.tar.xz
