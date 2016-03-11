b339dd8023a569796488e46d748120a07509a8c9
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-03-11 01:23:31 -0800

    removed wget from link replacement

--- bin/replace_starthardware_weblink.sh
+++ bin/replace_starthardware_weblink.sh
@@ -18,33 +18,7 @@ do
     fi
     file=`echo "$file" | sed -e 's/[.][/]//g'`
     file_echoed=false
-    for page in `../../bin/replace_starthardware_in_file.py "$file"`
-    do
-      if [ $file_echoed == false ]
-      then
-        echo "$file"
-        file_echoed=true
-      fi
-      echo -n "- $page ... "
-      if ! echo "$page" | grep -q -x "$pages_done"
-      then
-        if wget -N -c -p -q \
-                --header='User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0' \
-                --header='Cookie: _icl_current_language=de' \
-                --header="Referer: http://$file" \
-                "$page"
-        then
-          echo "ok"
-        else
-          echo "fail"
-        fi
-        changed=true
-        pages_done="$pages_done
-$page"
-      else
-        echo "done"
-      fi 
-    done
+    ../../bin/replace_starthardware_in_file.py "$file"
   done
 done
 
